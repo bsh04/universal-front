d0e6c7c27537ba7d960f60c7f33b6ec6efb46c04
Владимиров Михаил <vladimirov@buroburo.ru>
2020-06-16 09:40:18 +0700

    fix error with last product element

--- src/components/public/product_list.js
+++ src/components/public/product_list.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import request from "../../services/ajaxManager";
 import {connect} from "react-redux";
-import { withRouter } from "react-router";
+import {withRouter} from "react-router";
 import {Helmet} from "react-helmet";
 import Breadcrumbs from '../breadcrumbs';
 
@@ -28,9 +28,9 @@ class ProductList extends Component {
     }
 
     setCategory(cat) {
-        if(cat === 'new') {
+        if (cat === 'new') {
             return 'Новые товары';
-        } else if(cat === 'stock'){
+        } else if (cat === 'stock') {
             return 'Товары по акции';
         } else if (this.state.products && this.state.products[0]) {
             return this.state.products[0].category.title;
@@ -39,7 +39,7 @@ class ProductList extends Component {
 
     componentWillMount() {
         this.handleGet(this.props.match.params.category);
-        
+
         if (this.props.token !== false) {
 
             let _this = this;
@@ -55,25 +55,18 @@ class ProductList extends Component {
         }
     }
 
-    componentDidMount() {
-        let path = this.setCategory(this.props.match.params.category) 
-        this.setState({
-            path: path
-        })
-    }
-
     componentWillReceiveProps(props) {
         this.setState({viewCount: 30});
         this.handleGet(props.match.params.category);
-        
-        let path = this.setCategory(props.match.params.category) 
+
+        let path = this.setCategory(props.match.params.category)
         this.setState({
             path: path
         })
     }
 
     handleGet(cat) {
-        
+
         let obj = {
             cat: cat,
             limit: this.state.limit,
@@ -81,13 +74,13 @@ class ProductList extends Component {
             sort: `${this.state.sort[1]}`
         }
         let str = "";
-        for(let key in obj) {
+        for (let key in obj) {
             if (str != "") {
                 str += "&";
             }
             str += key + "=" + encodeURIComponent(obj[key]);
         }
-        
+
         let _this = this;
         let res;
         if (cat === 'search') {
@@ -103,22 +96,22 @@ class ProductList extends Component {
                     },
                     {},
                     function (response) {
+                        response.splice(-1, 1);
                         _this.setState({products: response, request: false});
                     },
                 );
             }
-        }
-        else {
+        } else {
             request(
                 'product/' + cat + '?' + str + (this.props.match.params.search ? '&data=' + this.props.match.params.search : ''),
                 'GET',
                 null,
                 {},
                 function (response) {
-                    
+                    response.splice(-1, 1);
                     _this.setState({products: response}, () => {
                         _this.setState({
-                            path: _this.setCategory(_this.props.match.params.category) 
+                            path: _this.setCategory(_this.props.match.params.category)
                         })
                     });
                 },
@@ -131,11 +124,10 @@ class ProductList extends Component {
         let result;
 
         if (this.isFavorite(obj)) {
-            result =  this.state.favorites.filter(item => {
+            result = this.state.favorites.filter(item => {
                 return obj.id !== item.id;
             });
-        }
-        else {
+        } else {
             result = arr;
             result.push(obj);
         }
@@ -144,7 +136,7 @@ class ProductList extends Component {
     }
 
     isFavorite(obj) {
-        let result =  this.state.favorites.filter(item => {
+        let result = this.state.favorites.filter(item => {
             return obj.id === item.id;
         });
 
@@ -155,22 +147,26 @@ class ProductList extends Component {
         let limit = e.target.getAttribute('data');
         this.setState({
             limit: limit
-        }, () => {this.handleGet(this.props.match.params.category)})
+        }, () => {
+            this.handleGet(this.props.match.params.category)
+        })
     }
     setSort = (e) => {
         let sort = e.target.getAttribute('data').split(',');
         this.setState({
             sort: sort
-        }, () => {this.handleGet(this.props.match.params.category)})
+        }, () => {
+            this.handleGet(this.props.match.params.category)
+        })
     }
 
     sortSelectedLabel() {
         let param = this.state.sort.join('-')
 
-        switch(param) {
+        switch (param) {
             case 'title-asc':
                 return 'названию А-Я'
-            case 'title-desc': 
+            case 'title-desc':
                 return 'названию Я-А'
             case 'price-asc':
                 return 'цене по возрастанию'
@@ -187,89 +183,94 @@ class ProductList extends Component {
                     <meta name="viewport" content="width=device-width, initial-scale=1"/>
                     <meta name="theme-color" content="#000000"/>
                     <title>Каталог - Универсал</title>
-                    <meta name="keywords" content="купить хозтовары, хозяйственные товары, бытовые товары, хозяйственно-бытовые товары, товары для дома"/>
-                    <meta name="description" content="Товары для дома, хозяйственные товары, спец. одежда и многое другое!"/>
-                    <meta property="og:description" content="Множество товаров для дома, хозяйства, авто и многого другого!"/>
+                    <meta name="keywords"
+                          content="купить хозтовары, хозяйственные товары, бытовые товары, хозяйственно-бытовые товары, товары для дома"/>
+                    <meta name="description"
+                          content="Товары для дома, хозяйственные товары, спец. одежда и многое другое!"/>
+                    <meta property="og:description"
+                          content="Множество товаров для дома, хозяйства, авто и многого другого!"/>
                     <meta property="og:title" content="Каталог"/>
                     <meta property="og:url" content="https://universal.tom.ru/catalog/*"/>
                 </Helmet>
-                
-                {this.state.path 
-                ? <Breadcrumbs 
-                    path={[
-                        { title: this.state.path }
-                    ]} /> : null}
+
+                {this.state.path
+                    ? <Breadcrumbs
+                        path={[
+                            {title: this.state.path}
+                        ]}/> : null}
                 <div className="products-toolbar mb-2 col-12">
                     <ul className="products-toolbar-group row justify-content-between" style={{paddingRight: 0}}>
                         <ul className="row col-xl-3 col-lg-3 col-md-3 col-sm-12 justify-content-center">
                             <li className="products-toolbar-item mr-2">
                                 <a href="#"
-                                    className={this.state.cardView === 'tile' ? 'text-success' : 'text-secondary'}
-                                    onClick={() => {
-                                        this.setState({
-                                            cardView: 'tile'
-                                        })
-                                    }}>
+                                   className={this.state.cardView === 'tile' ? 'text-success' : 'text-secondary'}
+                                   onClick={() => {
+                                       this.setState({
+                                           cardView: 'tile'
+                                       })
+                                   }}>
                                     <i className="fa fa-th"></i>
                                 </a>
                             </li>
                             <li className="products-toolbar-item">
                                 <a href="#"
-                                    className={this.state.cardView === 'list' ? 'text-success' : 'text-secondary'}
-                                    onClick={() => {
-                                        this.setState({
-                                            cardView: 'list'
-                                        })
-                                    }}>
-                                        <i className="fa fa-list"></i>
-                                    </a>
+                                   className={this.state.cardView === 'list' ? 'text-success' : 'text-secondary'}
+                                   onClick={() => {
+                                       this.setState({
+                                           cardView: 'list'
+                                       })
+                                   }}>
+                                    <i className="fa fa-list"></i>
+                                </a>
                             </li>
                         </ul>
                         <ul className="row col-xl-8 col-lg-8 col-md-8 col-sm-12 justify-content-center">
                             <li className="products-toolbar-item mr-1">
                                 <div className="dropdown">
                                     <span className="products-toolbar-item__text">Товаров на странице: </span>
-                                    <a className="dropdown-toggle" 
-                                        href="#" 
-                                        role="button" 
-                                        id="dropdownMenuLink" 
-                                        data-toggle="dropdown" 
-                                        aria-haspopup="true" 
-                                        aria-expanded="false">
-                                        <span className="products-toolbar-item__selected">{this.state.limit ? this.state.limit : 'Все'}</span>
+                                    <a className="dropdown-toggle"
+                                       href="#"
+                                       role="button"
+                                       id="dropdownMenuLink"
+                                       data-toggle="dropdown"
+                                       aria-haspopup="true"
+                                       aria-expanded="false">
+                                        <span
+                                            className="products-toolbar-item__selected">{this.state.limit ? this.state.limit : 'Все'}</span>
                                     </a>
                                     <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
                                         <a className="dropdown-item" href="#" data="50"
-                                            onClick={(e) => this.setLimit(e)}>50</a>
+                                           onClick={(e) => this.setLimit(e)}>50</a>
                                         <a className="dropdown-item" href="#" data="100"
-                                            onClick={(e) => this.setLimit(e)}>100</a>
+                                           onClick={(e) => this.setLimit(e)}>100</a>
                                         <a className="dropdown-item" href="#" data={null}
-                                            onClick={(e) => this.setLimit(e)}>Все</a>
+                                           onClick={(e) => this.setLimit(e)}>Все</a>
                                     </div>
                                 </div>
-                                
+
                             </li>
                             <li className="products-toolbar-item mr-1">
                                 <div className="dropdown">
                                     <span className="products-toolbar-item__text">Сортировать по: </span>
-                                    <a className="dropdown-toggle products-toolbar-item__text" 
-                                        href="#" 
-                                        role="button" 
-                                        id="dropdownMenuLink" 
-                                        data-toggle="dropdown" 
-                                        aria-haspopup="true" 
-                                        aria-expanded="false">
-                                        <span className="products-toolbar-item__selected">{this.sortSelectedLabel()}</span>
+                                    <a className="dropdown-toggle products-toolbar-item__text"
+                                       href="#"
+                                       role="button"
+                                       id="dropdownMenuLink"
+                                       data-toggle="dropdown"
+                                       aria-haspopup="true"
+                                       aria-expanded="false">
+                                        <span
+                                            className="products-toolbar-item__selected">{this.sortSelectedLabel()}</span>
                                     </a>
                                     <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
                                         <a className="dropdown-item" href="#" data="title,asc"
-                                            onClick={(e) => this.setSort(e)}>названию А-Я</a>
+                                           onClick={(e) => this.setSort(e)}>названию А-Я</a>
                                         <a className="dropdown-item" href="#" data="title,desc"
-                                            onClick={(e) => this.setSort(e)}>названию Я-А</a>
+                                           onClick={(e) => this.setSort(e)}>названию Я-А</a>
                                         <a className="dropdown-item" href="#" data="price,asc"
-                                            onClick={(e) => this.setSort(e)}>цене по возрастанию</a>
+                                           onClick={(e) => this.setSort(e)}>цене по возрастанию</a>
                                         <a className="dropdown-item" href="#" data="price,desc"
-                                            onClick={(e) => this.setSort(e)}>цене по убыванию</a>
+                                           onClick={(e) => this.setSort(e)}>цене по убыванию</a>
                                     </div>
                                 </div>
                             </li>
@@ -280,25 +281,26 @@ class ProductList extends Component {
                     {this.state.products.length > 0 ? this.state.products.map((item, key) => {
                         if (key < this.state.viewCount) {
                             return (
-                                <div className={this.state.cardView === 'tile' ? "col-md-3" : 'col-md-12'} 
-                                      style={{paddingBottom: '10px'}} 
-                                      key={key}>
+                                <div className={this.state.cardView === 'tile' ? "col-md-3" : 'col-md-12'}
+                                     style={{paddingBottom: '10px'}}
+                                     key={key}>
                                     <Card item={item} key={item.id} update={this.updateFav}
                                           favorite={this.isFavorite(item) ? true : false}
                                           cardView={this.state.cardView}/>
                                 </div>
                             );
-                        }
-                        else {
+                        } else {
                             return;
                         }
                     }) : <p className={'text-center'}>Товары не найдены</p>}
                 </div>
-                { this.state.viewCount < this.state.products.length ?
+                {this.state.viewCount < this.state.products.length ?
                     <button className={'btn btn-success'}
-                            onClick={() => {this.setState({viewCount: (this.state.viewCount + 30)})}}>
+                            onClick={() => {
+                                this.setState({viewCount: (this.state.viewCount + 30)})
+                            }}>
                         <i className={'fa fa-cloud-download-alt'}> <span>Загрузить еще</span></i>
-                    </button> : null }
+                    </button> : null}
             </div>
         );
     }
